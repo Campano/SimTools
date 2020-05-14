c954f12936007ebbde468df679da4a9a6b5a134e
designer (Designer) <no-reply@simplicite.fr>
2020-05-14 03:03:25 +0200

    forceCreateObject

--- src/com/simplicite/commons/DesignerTools/DsnTool.java
+++ src/com/simplicite/commons/DesignerTools/DsnTool.java
@@ -3,6 +3,8 @@ package com.simplicite.commons.DesignerTools;
 import java.util.*;
 import com.simplicite.util.*;
 import com.simplicite.util.tools.*;
+import com.simplicite.util.tools.BusinessObjectTool;
+import com.simplicite.util.exceptions.CreateException;
 import java.io.IOException;
 import org.json.JSONObject;
 
@@ -11,6 +13,7 @@ import org.json.JSONObject;
  */
 public class DsnTool implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
+	private static final String DEFAULT_USER_MODULE="ApplicationUsers";
 	
 	/**
 	 * To be called by getTargetObject
@@ -33,6 +36,18 @@ public class DsnTool implements java.io.Serializable {
 		return null;		
 	}
 	
+	public static void forceCreateObject(ObjectDB obj, Grant g){
+		boolean[] crud = g.changeAccess(obj.getName(), true, true, false, false);
+		BusinessObjectTool bot = new BusinessObjectTool(obj);
+		try{
+			bot.create();
+		}
+		catch(CreateException e){
+			AppLog.error(DsnTool.class, "createObject", e.getMessage(), e, g);
+		}
+		g.changeAccess(obj.getName(), crud);
+	}
+	
 	public static byte[] html2pdf(String html) throws IOException{
 		String url = "https://wkhtml2pdf.dev.simplicite.io/";
 		String user = null;
@@ -44,18 +59,16 @@ public class DsnTool implements java.io.Serializable {
 		return Tool.readUrlAsByteArray(url, user, password, postData.toString(), headers, encoding);
 	}
 	
-	public static void setRespList(String userId, List<String> newGroupsList, String userModule){
+	public static void setRespList(String userId, List<String> newGroupsList){
 		List<String> oldGroupsList = getRespList(userId);
-		
 		// remove old unused groups
 		for(String oldGroup : oldGroupsList)
 			if(!newGroupsList.contains(oldGroup))
-				Grant.removeResponsibility(userId, oldGroup);
-				
+				Grant.removeResponsibility(userId, oldGroup);		
 		// add new missing groups
 		for(String newGroup : newGroupsList)
 			if(!oldGroupsList.contains(newGroup))
-				Grant.addResponsibility(userId, newGroup, Tool.getCurrentDate(), null, true, userModule);
+				Grant.addResponsibility(userId, newGroup, Tool.getCurrentDate(), null, true, DEFAULT_USER_MODULE);
 	}
 	
 	public static List<String> getRespList(String userId){
