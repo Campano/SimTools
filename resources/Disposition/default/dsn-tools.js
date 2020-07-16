38474a6231cf4aec5717065eda3b3071ff46d1ed
designer (Designer) <no-reply@simplicite.fr>
2020-07-16 16:52:23 +0200

    logout

--- resources/Disposition/default/dsn-tools.js
+++ resources/Disposition/default/dsn-tools.js
@@ -1,7 +1,7 @@
 var sim = (function(globals) {
     'use strict';
     var JS_BUNDLE = '/scripts/ajax/ajax-bundle.js';
-    var GLOBALS;
+    var GLOBALS = null;
     var TOKEN_STORAGE_KEY = 'sim-token';
     var APP = null;
 
@@ -16,50 +16,92 @@ var sim = (function(globals) {
         return () => new Promise(function(resolve, reject){
             var userCreds = params.hasOwnProperty('login') && params.hasOwnProperty('password') ? {login: params.login, password:params.password} : false;
             var token = params.hasOwnProperty('tryToken') &&  params.tryToken===true ? window.localStorage.getItem(TOKEN_STORAGE_KEY)||false : false;
-            if(APP!==null){
+            if(GLOBALS === null || Simplicite === undefined){
+                disp('SIM not initialized');
+                reject(); 
+            }
+            else if(APP!==null){
                 disp('Already connected'); 
-                resolve();
+                resolve(APP);
             }
             else if(token===false && userCreds===false){
                 disp('Impossible to connect without connection params'); 
                 reject();
             }
-            else{
-                var connectId = token ? '[token "'+TOKEN_STORAGE_KEY+'"]' : '[user "'+userCreds.login+'"]';
+            else if(token!==false){
+                disp('Start connection for token '+TOKEN_STORAGE_KEY);
                 APP = new Simplicite.Ajax(
                     GLOBALS.instanceRoot,
-                    GLOBALS.endpoint,
-                    token ? undefined : userCreds.login,
-                    token ? undefined : userCreds.password
+                    GLOBALS.endpoint
                 );
-
-                disp('Start connection for '+connectId);
                 APP.login(
-                    (s) => {
-                        disp('Successful connection for '+connectId+'. Saving token...');
-                        window.localStorage.setItem(TOKEN_STORAGE_KEY, s.authtoken);
-                        APP.getGrant(g => {
-                            disp('Got grant data for '+connectId+', user '+g.login+', responsibilities: '+JSON.stringify(APP.grant.responsibilities));
-                            resolve();
-                        });
-                    },
+                    connectSuccess(resolve),
                     () => {
-                        disp('Failed login for '+connectId);
+                        disp('Failed login for token. Removing token.');
                         window.localStorage.removeItem(TOKEN_STORAGE_KEY);
                         APP = null;
                         //retry if case failed for expired token
-                        if(token&&userCreds)
+                        if(userCreds){
+                            disp('UserCreds present, trying userCred Connection.');
                             connect(params)().catch(reject).then(resolve);
+                        }
                         else
                             reject();
-                        token&&userCreds ? connect(smp, userCreds, cb) : cb(false); 
                     },
                     token,
                     GLOBALS.params
                 );
             }
+            else{
+                disp('Start connection for user '+userCreds.login);
+                APP = new Simplicite.Ajax(
+                    GLOBALS.instanceRoot,
+                    GLOBALS.endpoint,
+                    userCreds.login,
+                    userCreds.password
+                );
+                APP.login(
+                    connectSuccess(resolve),
+                    () => {
+                        disp('Failed login');
+                        window.localStorage.removeItem(TOKEN_STORAGE_KEY);
+                        APP = null;
+                        reject();
+                    },
+                    false,
+                    GLOBALS.params
+                );
+            }
         });
+    }
 
+    function connectSuccess(callback){
+        return (s) => {
+            disp('Successful connection, saving token...');
+            window.localStorage.setItem(TOKEN_STORAGE_KEY, s.authtoken);
+            APP.getGrant(g => {
+                disp('Got grant data for user '+g.login+', responsibilities: '+JSON.stringify(APP.grant.responsibilities));
+                callback(APP);
+            });
+        };
+    }
+
+    function logout(){
+        return new Promise(function(resolve, reject){
+            disp("Logging out...")
+            if(APP!==null){
+                APP.logout(
+                    ()=>{
+                        window.localStorage.removeItem(TOKEN_STORAGE_KEY);
+                        APP=null;
+                        resolve();
+                    }, 
+                    reject
+                );
+            }
+            else
+                resolve();
+        });
     }
 
     function getApp(){
@@ -79,31 +121,15 @@ var sim = (function(globals) {
         });
     }
 
-    function disp(content){ //false, true, 'debug', 'container-id'
-        if(GLOBALS.log === false){
-            return;
-        }
-        if(GLOBALS.log === true || GLOBALS.log === undefined){
-            console.log(JSON.stringify(content, null, 4));
-            return;
-        }
-        else if(GLOBALS.log === 'debug'){
-            console.debug(JSON.stringify(content, null, 4));
-        }
-        else if(document.getElementById(GLOBALS.log) !== null){
-            var p = document.createElement("p");
-            p.textContent = content;
-            document.getElementById(GLOBALS.log)
-                .appendChild(p)
-                .appendChild(document.createElement("hr"));
-        }
+    function disp(content){
+        Utils.disp('[sim] '+content, GLOBALS.log);
     }
 
     return {
         init : init,
         connect : connect,
-        getApp: getApp,
-        disp: disp
+        logout: logout,
+        getApp: getApp
     };
 })();
 
@@ -122,7 +148,27 @@ var Utils = (function(){
 			link.target = "blank";
 			link.click();
 		}
-	}
+    }
+    
+    function disp(content, output){ //false, true, 'debug', 'container-id'
+        if(output === false){
+            return;
+        }
+        if(output === true || output === undefined){
+            console.log(content);
+            return;
+        }
+        else if(output === 'debug'){
+            console.debug(content);
+        }
+        else if(document.getElementById(output) !== null){
+            var p = document.createElement("p");
+            p.textContent = content;
+            document.getElementById(output)
+                .appendChild(p)
+                .appendChild(document.createElement("hr"));
+        }
+    }
 
 	function base64dataFromBoDoc(boDoc){
 		return 'data:'+boDoc.mime+';base64,'+boDoc.content;
@@ -157,6 +203,7 @@ var Utils = (function(){
 	return {
 		forceDownload: forceDownload,
 		base64dataFromBoDoc: base64dataFromBoDoc,
-		b64toBlob: b64toBlob
+        b64toBlob: b64toBlob,
+        disp: disp
 	};
 })();
